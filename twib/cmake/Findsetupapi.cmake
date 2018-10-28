1b5d96d2f2284321cc1ff878b954e1efe69a5985
misson20000 <xenotoad@xenotoad.net>
2018-10-28 15:34:35 -0700

    fix linking with libusbk

---
+++ twib/cmake/Findsetupapi.cmake
@@ -0,0 +1,26 @@
+if (SETUPAPI_LIBRARIES)
+  # in cache already
+  set(SETUPAPI_FOUND TRUE)
+else()
+  find_library(SETUPAPI_LIBRARY NAMES SetupAPI)
+
+  set(SETUPAPI_LIBRARIES ${SETUPAPI_LIBRARY})
+
+  if(SETUPAPI_LIBRARIES)
+    set(SETUPAPI_FOUND TRUE)
+  endif()
+
+  if(SETUPAPI_FOUND)
+    if (NOT SETUPAPI_FIND_QUIETLY)
+      message(STATUS "Found SetupAPI:")
+      message(STATUS " - Libraries: ${SETUPAPI_LIBRARIES}")
+    endif()
+  else()
+    if (SETUPAPI_FIND_REQUIRED)
+      message(FATAL_ERROR "Could not find SetupAPI")
+    endif()
+  endif()
+
+  # show only in the advanced view
+  mark_as_advanced(SETUPAPI_LIBRARIES)
+endif()
