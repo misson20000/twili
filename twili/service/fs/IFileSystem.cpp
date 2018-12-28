0b71231dc43a90df7051f57238a71d90cf972e3a
misson20000 <xenotoad@xenotoad.net>
2018-12-28 14:48:19 -0800

    Revert "remove some virt-exefs debug messages"
    This reverts commit 48d5a334765a1ea39b36dd726e057b5326d83791.

--- twili/service/fs/IFileSystem.cpp
+++ twili/service/fs/IFileSystem.cpp
@@ -28,6 +28,7 @@ IFileSystem::IFileSystem(trn::ipc::server::IPCServer *server) : trn::ipc::server
 }
 
 trn::ResultCode IFileSystem::Dispatch(trn::ipc::Message msg, uint32_t request_id) {
+	printf("IFileSystem receives %d\n", request_id);
 	switch(request_id) {
 	case 8:
 		return trn::ipc::server::RequestHandler<&IFileSystem::OpenFile>::Handle(this, msg);
